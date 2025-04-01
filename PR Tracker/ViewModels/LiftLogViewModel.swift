17e86fd8a05c08580890f52cdb78f1f1d92ae9f3
Jan Jędra <jan.batman4@gmail.com>
2025-04-01 19:21:24 +0200

    how does swiftData work

--- PR Tracker/ViewModels/LiftLogViewModel.swift
+++ PR Tracker/ViewModels/LiftLogViewModel.swift
@@ -1,58 +1,58 @@
+////
+////  LiftLogViewModel.swift
+////  PR Tracker
+////
+////  Created by Jan Jędra on 29/03/2025.
+////
 //
-//  LiftLogViewModel.swift
-//  PR Tracker
+//import Foundation
+//import SwiftData
 //
-//  Created by Jan Jędra on 29/03/2025.
-//
-
-import Foundation
-import SwiftData
-
-@MainActor
-class LiftLogViewModel: ObservableObject {
-    private let modelContext: ModelContext
-    
-    // Published properties for view updates
-    @Published var exercises: [Exercise] = []
-    @Published var sortCriteria: SortCriteria = .totalLiftsDescending
-    
-    enum SortCriteria {
-        case totalLiftsDescending
-        case nameAscending
-        // Add more cases as needed
-    }
-    
-    init(modelContext: ModelContext) {
-        self.modelContext = modelContext
-        fetchExercises()
-    }
-    
-    func fetchExercises() {
-        let descriptor = FetchDescriptor<Exercise>()
-        exercises = (try? modelContext.fetch(descriptor)) ?? []
-    }
-    
-    var sortedExercises: [Exercise] {
-        switch sortCriteria {
-        case .totalLiftsDescending:
-            return exercises.sorted {
-                $0.totalLifts != $1.totalLifts ?
-                $0.totalLifts > $1.totalLifts :
-                $0.hasGoal
-            }
-        case .nameAscending:
-            return exercises.sorted { $0.name < $1.name }
-        }
-    }
-    
-    // Add other business logic methods here
-    func addExercise(_ exercise: Exercise) {
-        modelContext.insert(exercise)
-        fetchExercises()
-    }
-    
-    func deleteExercise(_ exercise: Exercise) {
-        modelContext.delete(exercise)
-        fetchExercises()
-    }
-}
+//@MainActor
+//class LiftLogViewModel: ObservableObject {
+//    private let modelContext: ModelContext
+//    
+//    // Published properties for view updates
+//    @Published var exercises: [Exercise] = []
+//    @Published var sortCriteria: SortCriteria = .totalLiftsDescending
+//    
+//    enum SortCriteria {
+//        case totalLiftsDescending
+//        case nameAscending
+//        // Add more cases as needed
+//    }
+//    
+//    init(modelContext: ModelContext) {
+//        self.modelContext = modelContext
+//        fetchExercises()
+//    }
+//    
+//    func fetchExercises() {
+//        let descriptor = FetchDescriptor<Exercise>()
+//        exercises = (try? modelContext.fetch(descriptor)) ?? []
+//    }
+//    
+//    var sortedExercises: [Exercise] {
+//        switch sortCriteria {
+//        case .totalLiftsDescending:
+//            return exercises.sorted {
+//                $0.totalLifts != $1.totalLifts ?
+//                $0.totalLifts > $1.totalLifts :
+//                $0.hasGoal
+//            }
+//        case .nameAscending:
+//            return exercises.sorted { $0.name < $1.name }
+//        }
+//    }
+//    
+//    // Add other business logic methods here
+//    func addExercise(_ exercise: Exercise) {
+//        modelContext.insert(exercise)
+//        fetchExercises()
+//    }
+//    
+//    func deleteExercise(_ exercise: Exercise) {
+//        modelContext.delete(exercise)
+//        fetchExercises()
+//    }
+//}
